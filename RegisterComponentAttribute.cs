691d8505314d04d918494a089f2541c5b4aedca2
Aviuz <Aviuz@users.noreply.github.com>
2022-06-26 10:16:55 +0200

    Create RegisterComponentAttribute.cs

---
+++ RegisterComponentAttribute.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+    public class RegisterComponentAttribute : Attribute
+    {
+        public DependencyGroup Group { get; }
+        public Type InterfaceType { get; set; } = null;
+        public ServiceLifetime ServiceLifetime { get; set; }
+
+        public RegisterComponentAttribute(DependencyGroup group, ServiceLifetime serviceLifetime)
+        {
+            Group = group;
+            ServiceLifetime = serviceLifetime;
+        }
+
+        public RegisterComponentAttribute(DependencyGroup group, Type interfaceType, ServiceLifetime serviceLifetime)
+        {
+            Group = group;
+            InterfaceType = interfaceType;
+            ServiceLifetime = serviceLifetime;
+        }
+    }
+}
